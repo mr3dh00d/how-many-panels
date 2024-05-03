6fd0029ec08d42fe1b8ea80697edce8aed6ea22e
Dereck Pavez <dereck.pavez@outlook.com>
2024-05-03 12:01:43 -0400

    :sparkles: Desafío numero 1 completo

--- index.ts
+++ index.ts
@@ -2,7 +2,10 @@ interface dimensions {
     width: number,
     height: number
 }
-
+/**
+ * @description Función que retorna la cantidad de paneles que caen en un techo
+ * Considerando que el techo es de dimensiones "a" y "b" (rectángulo) y los paneles "x" y "y" (rectángulo)
+ */
 const getHowManyPanelsFit = ({
     roof,
     panel
@@ -10,14 +13,18 @@ const getHowManyPanelsFit = ({
     roof: dimensions,
     panel: dimensions
 }) => {
+    // Iniciamos el resultado en 0
     let result = 0
 
+    // determinamos si el panel cae de alguna forma en el techo, ya sea  horizontalmente o vertical
     if (panel.width > roof.width || panel.width > roof.height) {
         return result
     }
 
+    // Calculamos el area del techo y la dividimos en el area de los paneles
     result = (roof.width * roof.height) / (panel.width * panel.height)
 
+    // Cortamos el numero al entero
     return Math.floor(result)
 }
 
